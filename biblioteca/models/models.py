a4fffb21598471d97ec9d3fbfbda276c16da2523
VictorPinargote <joelpinargote76@gmail.com>
2025-10-15 06:55:14 -0500

    commit 3

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -1,18 +1,29 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
-
 
 class bibliotecaLibro(models.Model):
      _name = 'biblioteca.libro'
      _description = 'biblioteca.biblioteca'
-
-     name = fields.Char()
-     value = fields.Integer()
-     value2 = fields.Float(compute="_value_pc", store=True)
-     description = fields.Text()
+     _rec_name = 'firstname'
+     
+     firstname = fields.Char(string="Nombre libro")
+     author_id = fields.Many2one('biblioteca.autor', string="Autor Libro")
+     value = fields.Integer(string="numero Ejemplares")
+     value2 = fields.Float(compute="_value_pc", store=True, string="Costo")
+     description = fields.Text(string ="Resumen Libro")
 
      @api.depends('value')
      def _value_pc(self):
          for record in self:
              record.value2 = float(record.value) / 100
+             
+class bibliotecaAutor(models.Model):
+     _name = 'biblioteca.autor'
+     _description = 'biblioteca.autor'
+     
+    firstname = fields.Char()
+    lastname = fields.Char()
+    
+    @api.depends('firstname','lastname')
+    def _compute_display_name(self):
+        for record in self:
+            record.display_name = f"{record.firstname} {record.lastname}"
