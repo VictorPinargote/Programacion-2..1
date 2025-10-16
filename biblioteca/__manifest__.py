d0d2e62768b9b887b77e0e472632f4aea2510d66
VictorPinargote <joelpinargote76@gmail.com>
2025-10-16 10:28:16 -0500

    commit 7

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -1,33 +1,23 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "biblioteca",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'name': "Biblioteca (Estructura Definida)",
+    'summary': "Gestión completa y concisa de Préstamos, Multas, Usuarios y Libros.",
     'description': """
-Long description of module's purpose
+        Módulo funcional para una biblioteca, respetando la estructura de carpetas definida.
+        Incluye gestión de usuarios, libros, préstamos, y multas por vencimiento.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'author': "Tu Nombre",
+    'category': 'Biblioteca',
+    'version': '17.0.1.0.0',
+    'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        #'views/templates.xml',
+        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    #'demo': [
-     #   'demo/demo.xml',
-    #],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'AGPL-3',
 }
