6cc03d61900a229ad5813068f72890ba65ff50e0
VictorPinargote <joelpinargote76@gmail.com>
2025-10-22 21:23:56 -0500

    commit 8

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -1,23 +1,46 @@
+# -*- coding: utf-8 -*-
 {
-    'name': "Biblioteca (Estructura Definida)",
-    'summary': "Gestión completa y concisa de Préstamos, Multas, Usuarios y Libros.",
+    'name': "biblioteca",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
     'description': """
-        Módulo funcional para una biblioteca, respetando la estructura de carpetas definida.
-        Incluye gestión de usuarios, libros, préstamos, y multas por vencimiento.
+Long description of module's purpose
     """,
-    'author': "Tu Nombre",
-    'category': 'Biblioteca',
-    'version': '17.0.1.0.0',
-    'depends': ['base', 'mail'],
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    
+    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        #'views/template.xml'
+        'data/sequence.xml'
+        #'views/templates.xml',
     ],
-    'demo': [
-        'demo/demo.xml',
+    # only loaded in demonstration mode
+    #'demo': [
+     #   'demo/demo.xml',
+    #],
+    'assets': {
+    'web.assets_backend': [
+        'biblioteca/static/src/widgets/openlibrary_search_widget.js',
     ],
-    'installable': True,
+    },
     'application': True,
-    'license': 'AGPL-3',
+    'license': 'AGPL-3'
 }
+
+#12.5.0 Nos indica que cambio ha tenido el software, el tercero, son cosas pequeñas tipo errores ortograficos, errores que habia, el segundo es el cambio de la lógica, y el primero es un cambio mucho mas robuzto que cambia toda la estructura.*/
+#Depende de donde este el cambio, se hacen los test, si el tercero lo actualizo nomas, el segundo depende pero no hay que ser muchas pruebas el primero si o si toca hacer pruebas robustas*/
+
